148ec8e45e284f4b5052288815235d27624241ad
aramrw <aramitsa@gmail.com>
2023-09-18 13:42:19 -0700

    Added Watch Scripts and Fixed Bugs

---
+++ convertogg2mp3.ps1
@@ -0,0 +1,20 @@
+# Start your script (index.exe) from its folder in the background and hidden
+Start-Process -FilePath "index.exe" -WindowStyle Hidden
+
+# Start Anki.exe from the parent folder in the foreground
+Start-Process -FilePath "..\Anki.exe"
+
+do {
+    # Check if Anki.exe is running
+    $AnkiRunning = Get-Process -Name "Anki" -ErrorAction SilentlyContinue
+
+    # If Anki is not running, terminate index.exe
+    if (-not $AnkiRunning) {
+        Get-Process -Name "index" -ErrorAction SilentlyContinue | ForEach-Object { Stop-Process -Id $_.Id -Force }
+        break
+    }
+
+    # Sleep for a few seconds before checking again
+    Start-Sleep -Seconds 3  # Sleep for 5 seconds (you can adjust this time)
+
+} while ($true)
