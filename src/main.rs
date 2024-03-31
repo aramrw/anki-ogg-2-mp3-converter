3fbb974a2211b39226cb2d31e16fd609017ad7d5
aramrw <106574385+aramrw@users.noreply.github.com>
2024-03-31 00:33:39 -0700

    all main converting features added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use core::fmt;
 use std::{
-    env, fs::OpenOptions, io::{self, read_to_string, Error, Write}, path::{Path, PathBuf}, process::{Command, Output}
+    env, fs::{self, OpenOptions}, io::{self, read_to_string, Error, Write}, path::{Path, PathBuf}, process::{Command, Output}, time::Instant
 };
 
 use serde::{Deserialize, Serialize};
 use serde_json::{self, to_string_pretty};
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
@@ -20,19 +22,83 @@ struct ConfigErrors {
     current_dir: Option<String>,
 }
 
-fn main() {
-   match check_paths() {
-    Ok(config) => {
-        println!("{:?}", config);
-    }
-    Err(e) => {
-        eprintln!("{:#?}", e);
-    }
-   }
+impl std::fmt::Display for ConfigErrors {
+     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+         write!(f, "ConfigErrors: missing_key: {:?}, file_to_string: {:?}, open_as_file: {:?}, current_dir: {:?}", self.missing_key, self.file_to_string, self.open_as_file, self.current_dir)
+     }
 }
 
+impl std::error::Error for ConfigErrors {}
+
+#[tokio::main]
+async fn main() {
+    let start = Instant::now();
+    let ogg_files = collect_ogg_files().unwrap();
+    let tasks_per_chunk = std::cmp::min(10, num_cpus::get());
+
+    for (index, chunk) in ogg_files.chunks(tasks_per_chunk).enumerate() {
+        let duration = start.elapsed().as_secs();
+        let minutes = duration / 60;
+        let seconds = duration % 60;
+        if minutes == 0 {
+            print!("\r{}: {}s", index, seconds);
+        } else {
+            print!("\r{}: {}m{}s", index, minutes, seconds);
+        }
+        io::stdout().flush().unwrap(); 
+
+        let mut tasks = Vec::new();
+        for file_path in chunk {
+            let file_path = file_path.clone();
+            let task = tokio::spawn(async move {
+                //println!("{}", file_path);
+                
+                convert_ogg_to_mp3(&file_path).await.unwrap();
+            });
+            tasks.push(task);
+        }
+        futures::future::join_all(tasks).await;
+    }
+
+    let duration = start.elapsed().as_secs();
+    let minutes = duration / 60;
+    let seconds = duration % 60;
+        
+    if minutes == 0 {
+        println!("\n\nSuccessfully converted {} ogg files in {} seconds!", ogg_files.len(), seconds);
+    } else {
+        println!("\n\nSuccessfully converted {} ogg files in {} minutes and {} seconds!", ogg_files.len(), minutes, seconds);
+    }
+}
+
+fn collect_ogg_files() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    match check_paths() {
+        Ok(config) => {
+            let mut ogg_files = Vec::new();
+            println!("\n\nReading directory: {}", config.collections_path);
+            let path = config.collections_path;
+            let collection_media_folder = std::fs::read_dir(&path).unwrap();
+            for entry in collection_media_folder {
+                let entry = entry?;
+                let path = entry.path();
+                if let Some(extension) = path.extension() {
+                    if extension == "ogg" {
+                        //println!("{:?}", path.clone());
+                        ogg_files.push(path.to_string_lossy().into_owned());
+                    }
+                }
+            }
+            Ok(ogg_files)
+        }
+        Err(e) => {
+            Err(Box::new(e))
+        }
+    }
+}
+
+
 fn ask_collection_folder(current_dir: PathBuf) -> Result<Config, io::Error> {
-     let mut input = String::new();
+        let mut input = String::new();
                         println!("Example media folder: C:\\Users\\exampleUser\\AppData\\Roaming\\Anki2\\user1\\collection.media");
                         println!("Enter your collection.media folder path: ");
      io::stdin().read_line(&mut input)?;
@@ -52,7 +118,7 @@ fn ask_collection_folder(current_dir: PathBuf) -> Result<Config, io::Error> {
 fn check_paths() -> Result<Config, ConfigErrors> {
     let path = "./config.json";
     let file_exists = Path::new(path).exists();
-    let file = OpenOptions::new().append(true).create(true).open(path);
+    let file = OpenOptions::new().append(true).read(true).create(true).open(path);
 
     match file {
         Ok(mut config_file) => {
@@ -128,13 +194,25 @@ fn check_paths() -> Result<Config, ConfigErrors> {
 }
 
 
-fn convert_ogg_to_mp3(ogg_path: &str, output_path: &str) -> Result<Output, Error> {
+async fn convert_ogg_to_mp3(ogg_path: &str) -> Result<Output, Error> {
+    let mut output_path = String::new();
+        if let Some((left, _right)) = &ogg_path.rsplit_once(".") {
+            let joined = format!("{}.mp3", left);
+            output_path = joined;
+        }
     let output = Command::new("ffmpeg")
-        .args(&["-i", ogg_path, "-codec:a", "libmp3lame", output_path])
+        .args(&["-i", ogg_path, "-codec:a", "libmp3lame", &output_path])
         .output();
 
     match output {
-        Ok(output) => Ok(output),
+        Ok(output) => {
+            // delete the ogg file after conversion
+            if Path::new(&output_path).exists() {
+                fs::remove_file(ogg_path)?;
+            }
+            
+            Ok(output)
+        },
         Err(e) => {
             eprint!("Failed to execute command: {}", e);
             return Err(e);
